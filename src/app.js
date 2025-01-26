5d07df17d23a70e4d3d050d8973dcdca30899fce
Kanwar <kkaur531@myseneca.ca>
2025-01-25 20:11:53 -0500

    Updated

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const compression = require('compression');
 
 // author and version from our package.json file
 // TODO: make sure you have updated your name in the `author` section
-const { author, version } = require('../package.json');
+//const { author, version } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
