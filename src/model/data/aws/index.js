faa8bcadde86a4bb3be252320a3ec03593a30da6
Kanwar <kkaur531@myseneca.ca>
2025-03-31 20:11:18 -0400

    SOlving errors

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -1,7 +1,29 @@
+const MemoryDB = require('../memory/memory-db');
 const s3Client = require('./s3Client');
-const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand} = require('@aws-sdk/client-s3');
 const logger = require('../../../logger');
 
+const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand} = require('@aws-sdk/client-s3');
+
+// Create two in-memory databases: one for fragment metadata and the other for raw data
+const data = new MemoryDB();
+const metadata = new MemoryDB();
+
+// Write a fragment's metadata to memory db. Returns a Promise<void>
+function writeFragment(fragment) {
+  // Simulate db/network serialization of the value, storing only JSON representation.
+  // This is important because it's how things will work later with AWS data stores.
+  const serialized = JSON.stringify(fragment);
+  return metadata.put(fragment.ownerId, fragment.id, serialized);
+}
+
+// Read a fragment's metadata from memory db. Returns a Promise<Object>
+async function readFragment(ownerId, id) {
+  // NOTE: this data will be raw JSON, we need to turn it back into an Object.
+  // You'll need to take care of converting this back into a Fragment instance
+  // higher up in the callstack.
+  const serialized = await metadata.get(ownerId, id);
+  return typeof serialized === 'string' ? JSON.parse(serialized) : serialized;
+}
 
 // Writes a fragment's data to an S3 Object in a Bucket
 // https://github.com/awsdocs/aws-sdk-for-javascript-v3/blob/main/doc_source/s3-example-creating-buckets.md#upload-an-existing-object-to-an-amazon-s3-bucket
@@ -74,6 +96,20 @@ async function readFragmentData(ownerId, id) {
   }
 }
 
+// Get a list of fragment ids/objects for the given user from memory db. Returns a Promise
+async function listFragments(ownerId, expand = false) {
+  const fragments = await metadata.query(ownerId);
+  const parsedFragments = fragments.map((fragment) => JSON.parse(fragment));
+
+  // If we don't get anything back, or are supposed to give expanded fragments, return
+  if (expand || !fragments) {
+    return parsedFragments;
+  }
+
+  // Otherwise, map to only send back the ids
+  return parsedFragments.map((fragment) => fragment.id);
+}
+
 // Delete a fragment's metadata and data from S3. Returns a Promise
 async function deleteFragment(ownerId, id) {
   const params = {
@@ -90,6 +126,9 @@ async function deleteFragment(ownerId, id) {
   }
 }
 
+module.exports.listFragments = listFragments;
+module.exports.writeFragment = writeFragment;
+module.exports.readFragment = readFragment;
 module.exports.writeFragmentData = writeFragmentData;
 module.exports.readFragmentData = readFragmentData;
 module.exports.deleteFragment = deleteFragment;
